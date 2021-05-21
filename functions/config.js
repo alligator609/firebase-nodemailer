c75056e03abe9aa56e2e975572094daa31964c75
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-05-21 10:30:53 +0600

    first commit

---
+++ functions/config.js
@@ -0,0 +1,11 @@
+var config ={};
+
+    config.Host=  'mail.cogent.space';
+    config.Port=  465,
+    config.UseSSL=  true,
+    config.Name=  'PaySEC Notification';
+    config.Username=  'notify@cogent.space';
+    config.EmailAddress=  'notify@cogent.space';
+    config.Password=  '+7k&Xp-&1)wS';
+
+    module.exports=config;
